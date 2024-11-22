bac2598122747bf75abcfe2f408e129f438babcc
e43 <drakermaniak@gmail.com>
2024-11-22 16:33:25 +0100

    exo 2

--- SLAM/Site/Xampp/docs/pages/td5/Font-tester.php
+++ SLAM/Site/Xampp/docs/pages/td5/Font-tester.php
@@ -1,2 +1,10 @@
+<span style="color:<?=$_GET['color']??'black'?>; font-size:<?=$_GET['size']??'12'?>"</span>
+<div><?=$_GET['message']??"message par défaut";?></div>
+
 <?php
+echo '<a href="Font-tester.php">test</a><br>';
+echo '<a href="Font-tester.php?message=OUI&color=red&size=15">1</a><br>';
+echo '<a href="Font-tester.php?message=NON&color=green&size=30">2</a><br>';
+echo '<a href="Font-tester.php?message=AHAHAH&color=blue&size=50">3</a>';
+?>
 
--- SLAM/Site/Xampp/docs/pages/td5/Message.php
+++ SLAM/Site/Xampp/docs/pages/td5/Message.php
@@ -5,3 +5,5 @@ echo '<a href="Message.php?">test</a><br>';
 echo '<a href="Message.php?message=OUI">1</a><br>';
 echo '<a href="Message.php?message=NON">2</a><br>';
 echo '<a href="Message.php?message=AHAHAH">3</a>';
+?>
+
