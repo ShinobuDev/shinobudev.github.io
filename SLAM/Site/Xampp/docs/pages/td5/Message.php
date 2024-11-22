eacc35d137358f0c3f55649a55690c28b3f46f2e
e43 <drakermaniak@gmail.com>
2024-11-22 15:35:43 +0100

    exo 1

---
+++ SLAM/Site/Xampp/docs/pages/td5/Message.php
@@ -0,0 +1,7 @@
+<?php
+
+echo $_GET['message']??"message par défaut<br>";
+echo '<a href="Message.php?">test</a><br>';
+echo '<a href="Message.php?message=OUI">1</a><br>';
+echo '<a href="Message.php?message=NON">2</a><br>';
+echo '<a href="Message.php?message=AHAHAH">3</a>';
