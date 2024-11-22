e11efed471ce50ed255bd02ccf2d713941dc1cd1
e43 <drakermaniak@gmail.com>
2024-11-22 16:47:06 +0100

    exo 3

--- SLAM/Site/Xampp/docs/pages/td5/Font-tester-form.php
+++ SLAM/Site/Xampp/docs/pages/td5/Font-tester-form.php
@@ -1,1 +1,11 @@
-<?php
+<form>
+    <label for="message">Message</label><br>
+    <input type="text" id="message" name="message"><br>
+    <label for="size">taille</label><br>
+    <input type="number" id="size" name="size"><br>
+    <label for="color">couleur</label><br>
+    <input type="text" id="color" name="color"><br><br>
+    <input type="submit">
+</form>
+<span style="color:<?=$_GET['color']??'black'?>; font-size:<?=$_GET['size']??'12'?>"</span>
+<div><?=$_GET['message']??"message par défaut";?></div>
